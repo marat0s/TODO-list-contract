52333fa0e362cb050f1667ca8cbaed3b0e2d220a
Marat Askarbek <96424646+marat0s@users.noreply.github.com>
2023-11-11 16:21:33 +0600

    Update TODO_list_contract.sol

--- TODO_list_contract.sol
+++ TODO_list_contract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-contract EnhancedTodoList {
+contract TodoList {
     struct TodoItem {
         string task;
         bool isCompleted;
